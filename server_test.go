903260434b6b2fcb981171fbc4366a2011dc572d
Guillaume R <guillaume.rondon@gmail.com>
2020-05-19 23:43:01 +0200

    Refactoring

--- server_test.go
+++ server_test.go
@@ -1,12 +1,28 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHello(t *testing.T) {
-    got := Hello("John")
-    want := "Hello, John"
-
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
+    assertCorrectMessage := func(t *testing.T, got, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
     }
+
+	t.Run("saying hello to people", func(t *testing.T) {
+
+		got := Hello("John")
+		want := "Hello, John"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
