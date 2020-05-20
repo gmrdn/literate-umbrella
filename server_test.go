67fa08950a04510ea7515e4949b11a0cd0a010da
Guillaume R <guillaume.rondon@gmail.com>
2020-05-20 21:41:57 +0200

    Multilanguage and refactoring

--- server_test.go
+++ server_test.go
@@ -14,15 +14,27 @@ func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
 
-		got := Hello("John")
+		got := Hello("John", "")
 		want := "Hello, John"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jean-Pierre", "French")
+		want := "Bonjour, Jean-Pierre"
 		assertCorrectMessage(t, got, want)
 	})
 }
